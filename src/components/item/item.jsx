a97ae317d21c8554992ab03f9f64e36162dd67b6
pav-nk <pav_nk@vk.com>
2025-03-27 17:36:14 +0300

    The item component update has been implemented

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,14 +1,54 @@
 import { Component } from "react";
 
 import "./item.css";
+import SwapiService from "../../services/swapi-service";
 
 export default class Item extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    item: null,
+  };
+
+  componentDidMount() {
+    this.updateitem();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.itemId !== prevProps.itemId) {
+      this.updateitem();
+    }
+  }
+
+  updateitem() {
+    const { itemId } = this.props;
+    if (!itemId) {
+      return;
+    }
+
+    this.swapiService.getCharacter(itemId).then((item) => {
+      this.setState({ item });
+    });
+  }
+
   render() {
+    const { item } = this.state;
+    if (!item) {
+      return <span>Select a item from a list</span>;
+    }
+
+    const { name, description, image } = item;
+
     return (
-      <div className="character card">
-        {/* <img className="person-image" src="" /> */}
+      <div className="item card">
+        <img className="item-image" src={image} alt="character" />
+
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
+          <p>
+            <b>Description </b>
+            {description}
+          </p>
         </div>
       </div>
     );
